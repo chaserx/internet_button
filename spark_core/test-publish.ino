2633ae79f9a6675e325c2ada836c498993db7db7
Chase Southard <chase.southard@gmail.com>
2015-01-27 23:27:51 -0500

    eh? let's see if this makes a difference

--- spark_core/test-publish.ino
+++ spark_core/test-publish.ino
@@ -1,18 +1,31 @@
 const int buttonPin = D2;
 const int ledPin = D7;
-int buttonState = 0;
+int buttonState = LOW;
+long lastDebounceTime = 0;
+long debounceDelay = 50;
+int lastButtonState = HIGH;
 
 void setup() {
-    pinMode(buttonPin, INPUT_PULLUP);
-    pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(ledPin, LOW);
-    buttonState = digitalRead(buttonPin);
-    if (buttonState == LOW)
-    {
+  digitalWrite(ledPin, LOW);
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == LOW) {
         digitalWrite(ledPin, HIGH);
         Spark.publish("button_status","closed");
+      }
     }
+  }
+  lastButtonState = reading;
 }
